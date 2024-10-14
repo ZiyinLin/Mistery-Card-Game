7421145d45446391c2f08869f6a912466f12cd2d
ZiyinLin <xuanbing1464129430@gmail.com>
2024-10-14 15:43:06 +0800

    correct initial version

--- PlayerAction.h
+++ PlayerAction.h
@@ -1,14 +1,30 @@
 #ifndef PLAYERACTION_H
 #define PLAYERACTION_H
 
-enum Action{
+#include "Card.h"
+#include <string>
 
+enum Action{
+    dicard_card,                 // to discard cards in 1st phrase
+    play_card,                   // to play a card in 2nd phrase
+    select_oriented_card,        // to select objective card of "JIE" and "du"
+    point_out_card,              // to point out a card in 3rd phrase
 };
 
 class PlayerAction{
 public:
-    // diffrent cards are played diffrently, therefore we have to override play function
-    virtual void play(Player* player_pointer, GameState& gameState, bool face_up);
+    PlayerAction(Action a, const std::string s) :
+    action_type(a), player_id(s){}
+
+
+
+private:
+    Action action_type;
+    std::string player_id;          // current player's id
+    card_pointer card;              // to choose your hand card
+    card_pointer target_card;       // to choose other's card
+    std::string target_player_id;
+    bool faceUp;
 };
 
 #endif
